3a7c9caae2b854abd956629619ad932a9738bf49
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2023-10-17 11:42:12 +0200

    add check_flush function

--- 13_7_lookup/src/lib.c
+++ 13_7_lookup/src/lib.c
@@ -6,6 +6,20 @@ char values_char[] = {'2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K'
 char suits[] = {CLUBS, DIAMONDS, HEARTS, SPADES};
 char suits_char[] = {'c', 'd', 'h', 's'};
 
+char check_flush(Card cards[], int no_elements)
+{
+    char suit;
+    char suits[4] = {0};
+
+    for (int i = 0; i < no_elements; i++) {
+        suit = cards[i].suit;
+        suits[suit]++;
+        if (suits[suit] == 5) return suit;
+    }
+
+    return -1;
+}
+
 void print_cards(Card cards[], int no_elements)
 {
     for (int i = 0; i < no_elements; i++) {
