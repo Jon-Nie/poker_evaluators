7c164646311d3338e1f3f65c83646fc51d362f12
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2023-10-17 11:20:05 +0200

    add lib src file: add values and suits arrays

---
+++ 13_7_lookup/src/lib.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "../include/lib.h"
+
+char values[] = {TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE};
+char values_char[] = {'2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'};
+char suits[] = {CLUBS, DIAMONDS, HEARTS, SPADES};
+char suits_char[] = {'c', 'd', 'h', 's'};
