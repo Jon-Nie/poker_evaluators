676b157cebddf3d6db01f65fe6c88c0af8a181c8
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2023-10-17 11:42:54 +0200

    add check_flush declaration and description

--- 13_7_lookup/include/lib.h
+++ 13_7_lookup/include/lib.h
@@ -31,6 +31,13 @@
 #endif
 
 /*
+check_flush takes an array of type Card and the number of elements in that array as arguments
+and returns the integer representation of the suit (0: clubs, 1: diamonds, 2: hearts, 3: spades)
+if the hand is a flush and -1 else.
+*/
+char check_flush(Card cards[], int no_elements);
+
+/*
 print_cards prints the string representation of each card in an array.
 The parameters have to be the card array of type Card and the number of elements in that array.
 */
