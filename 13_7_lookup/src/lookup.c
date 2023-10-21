d9659fb5ffe830db5efa131a827d0e90a6f96e69
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2023-10-21 14:18:32 +0200

    add error message when loading lookup tables, fix relative path

--- 13_7_lookup/src/lookup.c
+++ 13_7_lookup/src/lookup.c
@@ -6,11 +6,17 @@ void load_lookup_tables()
 {
     FILE* file;
 
-    file = fopen("../tables/flushes5.data", "rb");
-    fread(flushes5, sizeof(int), ARRSIZE_13_5, file);
+    file = fopen("tables/flushes5.data", "rb");
+    int success = fread(flushes5, sizeof(int), ARRSIZE_13_5, file);
     fclose(file);
+    if (success == 0) {
+        printf("Error: couldnt read flushes5 file.\n");
+    }
 
-    file = fopen("../tables/nonflushes5.data", "rb");
+    file = fopen("tables/nonflushes5.data", "rb");
     fread(nonflushes5, sizeof(int), ARRSIZE_13_5, file);
     fclose(file);
+    if (success == 0) {
+        printf("Error: couldnt read nonflushes5 file.\n");
+    }
 }
