c962f5ed252dd5ad4b7f6e02bcf23b86fb84ee0c
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2023-10-25 23:06:21 +0200

    rewrite bincoeff as explicit and remove recursion, build_active_bits_table: fix types

--- 64bit_integer/src/utils.c
+++ 64bit_integer/src/utils.c
@@ -3,18 +3,21 @@
 #include <stdint.h>
 #include "../include/utils.h"
 
+#define MAX_BITS    7
+
 uint8_t *build_active_bits_table()
 {
     uint8_t no_bits;
-    uint8_t number;
+    uint16_t number;
     uint8_t *active_bits = malloc(sizeof(uint8_t) * ARRSIZE_16);
 
-    for (int index = 0; index < ARRSIZE_16; index++) {
+    for (uint32_t index = 0; index < ARRSIZE_16; index++) {
         number = index;
         no_bits = 0;
-        while (number != 0) {
-            no_bits += (number & 1);
-            number >>= 1;
+        for (int bit = 0; bit < 16; bit++) {
+            if ((number & (1<<bit))) {
+                no_bits += 1;
+            }
         }
         active_bits[index] = no_bits;
     }
@@ -25,7 +28,26 @@ uint8_t *build_active_bits_table()
 uint64_t bincoeff(uint16_t n, uint16_t k)
 {
     if (k > n) return 0;
-    if (k == 0 || k == n) return 1;
 
-    return bincoeff(n-1, k-1) + bincoeff(n-1, k);
+    uint64_t combis = 1;
+    int stop;
+
+    if (k > (n - k)) {
+        stop = k;
+        k = n - k;
+    } else {
+        stop = n - k;
+    }
+
+    // numerator
+    while (n > stop) {
+        combis *= n--;
+    }
+
+    // demoninator
+    while (k >= 1) {
+        combis /= k--;
+    }
+
+    return combis;
 }
