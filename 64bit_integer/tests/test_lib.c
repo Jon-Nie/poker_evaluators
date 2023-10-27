b6bf799ac3d505efb8a63299d92f43af654d0ddd
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2023-10-27 23:23:31 +0200

    add lib.c tests

---
+++ 64bit_integer/tests/test_lib.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <assert.h>
+#include "../include/lib.h"
+
+int main()
+{
+    uint64_t* deck_list = init_deck();
+    uint64_t deck;
+    for (int i = 0; i < 52; i++) {
+        print_cards(deck_list[i]);
+        deck = deck | deck_list[i];
+    }
+
+    printf("All cards: ");
+    print_cards(deck);
+}
